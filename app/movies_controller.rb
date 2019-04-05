4665359c29aa5808d18947292812620e51ff91ff
Alyssa Brown <alyssa.brown@live.com>
2019-04-05 03:55:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -58,7 +58,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  Move.update (title: "Wat, huh?")
+  Movie.update (title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
