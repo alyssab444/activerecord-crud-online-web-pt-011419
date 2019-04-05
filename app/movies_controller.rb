ee82d17ad5263f528cf001fef6c887c5d0710533
Alyssa Brown <alyssa.brown@live.com>
2019-04-05 02:42:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,10 +23,10 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(title = "Home Alone", release_date= nil)
+def can_be_created_in_a_block(title = "Home Alone", release_date= "1990")
   Movie.create do |m|
    m.title 
-    m.release_date = "1990"
+    m.release_date 
   end
 end
 
