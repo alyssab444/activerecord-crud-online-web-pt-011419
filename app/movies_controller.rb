d3a08abef93eafecbdcec06b9047466d3d0ce9b8
Alyssa Brown <alyssa.brown@live.com>
2019-04-05 03:17:04 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,7 +26,7 @@ end
 def can_be_created_in_a_block(title= "Home Alone", release_date= "1990")
   Movie.create do |m|
    m.title = "The Room"
-    m.release_date = "1990"
+    m.release_date = "2003"
   end
 end
 
